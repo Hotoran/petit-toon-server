0ee2f9efab7d4cf6daa19219a195c63e91065859
Hotoran <hotoran0410@gmail.com>
2023-09-13 20:59:31 +0900

    fix: Cors 세팅

--- src/main/java/com/petit/toon/config/WebConfig.java
+++ src/main/java/com/petit/toon/config/WebConfig.java
@@ -18,7 +18,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 
     @Override
