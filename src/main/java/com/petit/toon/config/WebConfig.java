43b0d32065d2a9007d7a7d639213479b92c28017
Hotoran <hotoran0410@gmail.com>
2023-09-15 11:58:02 +0900

    fix: 복구

--- src/main/java/com/petit/toon/config/WebConfig.java
+++ src/main/java/com/petit/toon/config/WebConfig.java
@@ -18,8 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowCredentials(true);
+                .allowedMethods("*");
     }
 
     @Override
